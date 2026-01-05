d5f2729448e4ff881fa216a9cdc3c24ab0cb819a
Tim Triche, Jr. <tim.triche@gmail.com>
2026-01-05 14:47:40 -0500

    updated iSEEarchR with dryRun option

--- R/iSEEarchR.R
+++ R/iSEEarchR.R
@@ -3,7 +3,8 @@
 #' Note that this is a quick and dirty affair so don't expect much. 
 #'
 #' @param x             a SingleCellExperiment with UMAP and LSI reducedDims
-#' @param colorColumn   the colData column to color plots ("mitoCluster")
+#' @param colorColumn   the colData column to color plots ("Clusters")
+#' @param dryRun        just preprocess the data and exit without iSEE? (FALSE)
 #'
 #' @details presumes NMF and UMAP. you will almost certainly want to tweak this.
 #'
@@ -11,45 +12,49 @@
 #'
 #' @export
 #'
-iSEEarchR <- function(x, colorColumn = "Clusters") { 
+iSEEarchR <- function(x, colorColumn = "Clusters", dryRun=FALSE) { 
 
   # reason for this will become obvious 
   stopifnot(c("UMAP","LSI") %in% reducedDimNames(x))
 
   # if reducedDim(x) columns aren't already in colData(x), add them
-  for (rdim in reducedDimNames(x)) x <- .reducedDimsAsColData(x, rdim=rdim)
+  rdims <- intersect(c("LSI", "NMF"), reducedDimNames(x))
+  for (rdim in rdims) x <- .reducedDimsAsColData(x, rdim=rdim)
 
-  # launch
-  iSEE(x,
-     initial = 
-       list(UMAP = new("ReducedDimensionPlot",
-                       FontSize = 1.5,
-                       PointSize = 4,
-                       VisualBoxOpen = TRUE,
-                       ColorBy = "Column data",
-                       ColorByColumnData = colorColumn,
-                       Type = "UMAP"
-                       ),
-            FACTORS = new("ColumnDataPlot",
-                       YAxis = "TSSEnrichment",
-                       XAxis = "Column data",
-                       XAxisColumnData = "Sample",
-                       FontSize = 1.5,
-                       PointSize = 4,
-                       DataBoxOpen = TRUE,
-                       SelectionBoxOpen = TRUE,
-                       ColorBy = "Column data",
-                       ColorByColumnData = colorColumn,
-                       ColumnSelectionDynamicSource = TRUE,
-                       ColumnSelectionSource = "ReducedDimensionPlot1"
-                       ),
-            COLDAT = new("ColumnDataTable",
-                       SelectionBoxOpen = TRUE,
-                       ColumnSelectionDynamicSource = TRUE,
-                       ColumnSelectionSource = "ReducedDimensionPlot1"
-                       )
-            )
-     )
+  if (dryRun) {
+    message("Finished dry run, exiting.")
+  } else {
+    iSEE(x,
+       initial = 
+         list(UMAP = new("ReducedDimensionPlot",
+                         FontSize = 1.5,
+                         PointSize = 4,
+                         VisualBoxOpen = TRUE,
+                         ColorBy = "Column data",
+                         ColorByColumnData = colorColumn,
+                         Type = "UMAP"
+                         ),
+              FACTORS = new("ColumnDataPlot",
+                         YAxis = "TSSEnrichment",
+                         XAxis = "Column data",
+                         XAxisColumnData = "Sample",
+                         FontSize = 1.5,
+                         PointSize = 4,
+                         DataBoxOpen = TRUE,
+                         SelectionBoxOpen = TRUE,
+                         ColorBy = "Column data",
+                         ColorByColumnData = colorColumn,
+                         ColumnSelectionDynamicSource = TRUE,
+                         ColumnSelectionSource = "ReducedDimensionPlot1"
+                         ),
+              COLDAT = new("ColumnDataTable",
+                         SelectionBoxOpen = TRUE,
+                         ColumnSelectionDynamicSource = TRUE,
+                         ColumnSelectionSource = "ReducedDimensionPlot1"
+                         )
+              )
+       )
+  }
 }
 
 
