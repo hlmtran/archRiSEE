c2d2f19d84c1c602f68e1f876578230972dd0b03
Tim Triche, Jr <tim.triche@gmail.com>
2026-02-03 17:02:14 -0500

    add archRbarCodes function

---
+++ R/archRbarCodes.R
@@ -0,0 +1,22 @@
+#' extract unique barcodes from cellColData in an archR project
+#' 
+#' @param proj    the project
+#' @param splt    the split string ('#')
+#' @param pos     which piece to keep (2)
+#'
+#' @return        a character vector
+#'
+#' @examples
+#'
+#' inDEM <- archRbarCodes(MLLDEM)
+#' inK27 <- archRbarCodes(MLLK27)
+#' table(inDEM %in% inK27)
+#' table(inK27 %in% inDEM) 
+#'
+#' @export
+#'
+archRbarCodes <- function(proj, splt="#", pos=2)  { 
+
+  sapply(strsplit(rownames(proj@cellColData), splt), "[", pos)
+
+}
