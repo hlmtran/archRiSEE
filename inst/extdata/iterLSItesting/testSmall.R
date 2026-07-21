b22c9d89892eae8f35411b39ae6510f805b68582
hlmtran <hoangletranmai@gmail.com>
2026-07-21 17:25:54 -0400

    merging changes with Tim and remove 0 clumns to be consistent with ArchR

--- inst/extdata/iterLSItesting/testSmall.R
+++ inst/extdata/iterLSItesting/testSmall.R
@@ -2,9 +2,6 @@ library(archRiSEE)
 
 # test project
 library(ArchR)
-proj <- getTestProject()
-proj <- addIterativeLSI(proj, dimsToUse=1:5, varFeatures=1000, force=TRUE)
-#
 # Checking Inputs...
 # Detected less than 500 Cells.
 # `filterBias` disabled.
